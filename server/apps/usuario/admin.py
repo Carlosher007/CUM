894d521f74942d92795f0975192ce5a1a526dc50
GetialJuan <getial.juan@correounivalle.edu.co>
2023-05-16 18:21:17 -0500

    Add usuario JefeTaller model

--- server/apps/usuario/admin.py
+++ server/apps/usuario/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Gerente, JefeTaller
 
 # Register your models here.
+admin.site.register(Gerente)
+admin.site.register(JefeTaller)
--- server/apps/usuario/models.py
+++ server/apps/usuario/models.py
@@ -11,3 +11,13 @@ class Gerente(models.Model):
 
     class Meta:
         db_table = 'gerente'
+
+class JefeTaller(models.Model):
+    cedula = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
+    nombre_completo = models.CharField(max_length=50)
+    celular = models.CharField(max_length=10)
+    email = models.CharField(max_length=70)
+    password = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'jefe_taller'
