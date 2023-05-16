efd448014cf0f162e8c3e0effcd26276c042e85f
GetialJuan <getial.juan@correounivalle.edu.co>
2023-05-16 17:25:28 -0500

    Add Cliente - api - urls

--- server/cum_api/urls.py
+++ server/cum_api/urls.py
@@ -21,5 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.vehiculo.urls')),
+    path('api/', include('apps.cliente.urls')),
+
     path('docs/', include_docs_urls(title='CUM API'))
 ]
