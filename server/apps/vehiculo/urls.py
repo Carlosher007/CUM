b499189f03d2b04e4a7d96a9af50ad5eb21333c5
GetialJuan <getial.juan@correounivalle.edu.co>
2023-05-15 20:26:38 -0500

    Delete API_VERSION constant

--- server/apps/vehiculo/urls.py
+++ server/apps/vehiculo/urls.py
@@ -5,8 +5,7 @@ from apps.vehiculo import views
 router = routers.DefaultRouter()
 router.register(r'vehiculo', views.VehiculoView, 'vehiculo')
 
-API_VERSION = 'v0.0'
 
 urlpatterns = [
-    path('{}/'.format(API_VERSION), include(router.urls))
+    path('/', include(router.urls))
 ]
