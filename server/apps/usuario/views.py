59814454e27afb0d9c05702a91d049fce1198374
GetialJuan <getial.juan@correounivalle.edu.co>
2023-05-19 18:02:52 -0500

    Add ClienteApiView

--- server/apps/usuario/views.py
+++ server/apps/usuario/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Cliente
+from .serializers import ClienteSerializer
 
 # Create your views here.
+class ClienteAPIView(APIView):
+
+    def get(self, request):
+        clientes = Cliente.objects.all()
+        clientes_serializer = ClienteSerializer(clientes, many=True)
+        return Response(clientes_serializer.data)
+    
+    def post(self, request):
+        cliente_serializer = ClienteSerializer(data=request.data)
+        if cliente_serializer.is_valid():
+            cliente_serializer.save()
+            return Response(cliente_serializer.data)
+        return Response(cliente_serializer.errors)
