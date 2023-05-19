a900e17be831c57f533de81c302df7cc6c749e9f
GetialJuan <getial.juan@correounivalle.edu.co>
2023-05-19 18:00:44 -0500

    Add Cliente model

--- server/apps/usuario/models.py
+++ server/apps/usuario/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 from apps.sucursal.models import Sucursal
 
 # Create your models here.
@@ -21,3 +22,20 @@ class JefeTaller(models.Model):
 
     class Meta:
         db_table = 'jefe_taller'
+
+class Cliente(models.Model):
+    cedula = models.CharField(max_length=10, primary_key=True)
+    celular = models.CharField(max_length=10)
+    nombre_completo = models.CharField(max_length=100)
+    email = models.EmailField()
+    direccion = models.CharField(max_length=100)
+    contraseña = models.CharField(max_length=50, null=False)
+    sucursal = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'cliente'
+
+    def save(self, *args, **kwargs):
+        # Encriptamos la contraseña
+        self.contraseña = make_password(self.contraseña)
+        super(Cliente, self).save(*args, **kwargs)
