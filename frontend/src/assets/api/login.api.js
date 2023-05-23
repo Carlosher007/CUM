86bcad66a6f85c391ca20ff4632f8bd30cd9644d
Carlosher007 <carlos.hernandez.agudelo@correounivalle.edu.co>
2023-05-23 11:06:38 -0500

    El cliente puede ingresar al dashboard despues de loguearse

---
+++ frontend/src/assets/api/login.api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const loginApi = axios.create({
+  baseURL: 'http://localhost:8000/api/', // Ajusta la URL base según la configuración de tu backend de Django
+});
+
+export const loginUser = (credentials) => loginApi.post('login/', credentials);
