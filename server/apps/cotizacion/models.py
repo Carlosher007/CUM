afe2fb9c995ea113f8c4afd51a9f8c2d42816da3
GetialJuan <getial.juan@correounivalle.edu.co>
2023-06-13 20:23:56 -0500

    Add cotizacion models

--- server/apps/cotizacion/models.py
+++ server/apps/cotizacion/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class QuotationRequest(models.Model):
+    cliente = models.ForeignKey('usuario.User', 
+                             on_delete=models.CASCADE)
+    vehicle_sucursal = models.ForeignKey('sucursal.VehicleSucursal',
+                                         on_delete=models.CASCADE)
+    assigned = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'quotation_request'
+
+class AssignedQuotationRequest(models.Model):
+    quotation_request = models.OneToOneField(QuotationRequest,
+                                             on_delete=models.CASCADE,
+                                             primary_key=True)
+    vendedor = models.ForeignKey('usuario.User',
+                                 on_delete=models.CASCADE)
+    finished = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'assigned_quotation_request'
+
+class Quotation(models.Model):
+    assigned_quotation_request = models.ForeignKey(AssignedQuotationRequest,
+                                                   on_delete=models.CASCADE)
+    num_installments = models.PositiveSmallIntegerField()
+    initial_fee = models.PositiveIntegerField()
+    quota_value = models.PositiveIntegerField()
+    accepted = models.BooleanField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'quotation'
