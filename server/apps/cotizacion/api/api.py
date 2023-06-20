57647d04adb6454b8c6c5a68cb1fb490e928a8ae
Juan Sebastian Getial Getial <99220926+GetialJuan@users.noreply.github.com>
2023-06-20 16:38:51 +0000

    Add get_assigned_quotes_seller and client endpoints

--- server/apps/cotizacion/api/api.py
+++ server/apps/cotizacion/api/api.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from ..models import Quotation, AssignedQuote
 from .serializers import QuotationSerializer, AssignedQuoteSerializer
@@ -13,4 +14,16 @@ class AssignedQuoteApiView(viewsets.ModelViewSet):
     serializer_class = AssignedQuoteSerializer
     queryset = AssignedQuote.objects.all()
 
+    @action(detail=False, methods=['GET'], url_path='assigned-quotes-seller/(?P<seller>\w+)')
+    def get_assigned_quotes_seller(self, request, seller:str):
+        assigned_quotes = AssignedQuote.objects.filter(seller=seller)
+        assigned_quote_serializer = AssignedQuoteSerializer(assigned_quotes, many=True)
+        return Response(assigned_quote_serializer.data,
+                        status=status.HTTP_200_OK)
     
+    @action(detail=False, methods=['GET'], url_path='assigned-quotes-client/(?P<client>\w+)')
+    def get_assigned_quotes_client(self, request, client:str):
+        assigned_quotes = AssignedQuote.objects.filter(quotation__client=client)
+        assigned_quote_serializer = AssignedQuoteSerializer(assigned_quotes, many=True)
+        return Response(assigned_quote_serializer.data,
+                        status=status.HTTP_200_OK)
