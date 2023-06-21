1135be7def0ec6e618e5922ede5f47ba5cc8808d
GetialJuan <getial.juan@correounivalle.edu.co>
2023-06-20 21:18:07 -0500

    Update get_assigned_quotes for client and seller endpoints

--- server/apps/cotizacion/api/api.py
+++ server/apps/cotizacion/api/api.py
@@ -14,16 +14,28 @@ class AssignedQuoteApiView(viewsets.ModelViewSet):
     serializer_class = AssignedQuoteSerializer
     queryset = AssignedQuote.objects.all()
 
-    @action(detail=False, methods=['GET'], url_path='assigned-quotes-seller/(?P<seller>\w+)')
-    def get_assigned_quotes_seller(self, request, seller:str):
-        assigned_quotes = AssignedQuote.objects.filter(seller=seller)
-        assigned_quote_serializer = AssignedQuoteSerializer(assigned_quotes, many=True)
-        return Response(assigned_quote_serializer.data,
-                        status=status.HTTP_200_OK)
+    @action(detail=False, methods=['GET'], url_path='assigned-quotes-seller/(?P<seller>\w+)/(?P<state>\w+)')
+    def get_assigned_quotes_seller(self, request, seller:str, state:str):
+        states = ["IN_PROGRESS","CANCELLED", "ACCEPTED",
+        "FINISHED"]
+        if state in states:
+            assigned_quotes = AssignedQuote.objects.filter(seller=seller, state=state)
+            assigned_quote_serializer = AssignedQuoteSerializer(assigned_quotes, many=True)
+            return Response(assigned_quote_serializer.data,
+                            status=status.HTTP_200_OK)
+        
+        return Response({'error':'invalid state', 'states':states},
+                        status=status.HTTP_400_BAD_REQUEST)
     
-    @action(detail=False, methods=['GET'], url_path='assigned-quotes-client/(?P<client>\w+)')
-    def get_assigned_quotes_client(self, request, client:str):
-        assigned_quotes = AssignedQuote.objects.filter(quotation__client=client)
-        assigned_quote_serializer = AssignedQuoteSerializer(assigned_quotes, many=True)
-        return Response(assigned_quote_serializer.data,
-                        status=status.HTTP_200_OK)
+    @action(detail=False, methods=['GET'], url_path='assigned-quotes-client/(?P<client>\w+)/(?P<state>\w+)')
+    def get_assigned_quotes_client(self, request, client:str, state:str):
+        states = ["IN_PROGRESS","CANCELLED", "ACCEPTED",
+        "FINISHED"]
+        if state in states:
+            assigned_quotes = AssignedQuote.objects.filter(quotation__client=client, state=state)
+            assigned_quote_serializer = AssignedQuoteSerializer(assigned_quotes, many=True)
+            return Response(assigned_quote_serializer.data,
+                            status=status.HTTP_200_OK)
+        
+        return Response({'error':'invalid state', 'states':states},
+                        status=status.HTTP_400_BAD_REQUEST)
