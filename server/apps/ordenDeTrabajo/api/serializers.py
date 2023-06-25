91fba4a2749869965834239533e6cb6fe2b48e8f
GetialJuan <getial.juan@correounivalle.edu.co>
2023-06-24 22:57:32 -0500

    Update CreateWorkOrderSerializer

--- server/apps/ordenDeTrabajo/api/serializers.py
+++ server/apps/ordenDeTrabajo/api/serializers.py
@@ -1,10 +1,28 @@
+
 from rest_framework import serializers
+
 from ..models import WorkOrder
 
+from apps.sucursal.models import SucursalPart
+
 class CreateWorkOrderSerializer(serializers.ModelSerializer):
-    #sucursal_part_ids = serializers.ListField(
-    #    child=serializers.IntegerField(min_value=0), required=False
-    #    )
     class Meta:
         model = WorkOrder
-        fields = '__all__'
+        exclude = ['state']
+
+    def save(self, **kwargs):
+        print(self.validated_data)
+        work_order = WorkOrder.objects.create(
+            client_vehicle=self.validated_data['client_vehicle'],
+            date=self.validated_data['date'],
+            description=self.validated_data['description'],
+        )
+        
+        if 'parts' in self.validated_data:
+            for sucursal_part in self.validated_data['parts']:
+                if sucursal_part.quantity > 0:
+                    sucursal_part.quantity -= 1
+                    sucursal_part.save()
+                    work_order.parts.add(sucursal_part)
+
+        work_order.save()
