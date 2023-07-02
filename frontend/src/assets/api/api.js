bc8f8f3f19781f6491a9b48149f15cb39e18f648
Carlosher007 <carlos.hernandez.agudelo@correounivalle.edu.co>
2023-07-02 18:34:39 -0500

    Cambios en la url

--- frontend/src/assets/api/api.js
+++ frontend/src/assets/api/api.js
@@ -4,24 +4,23 @@ import { getTokenValue } from './tokenCookie';
 // https://cum-api-rest.onrender.com/api/
 
 const path = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://cum-api-rest.onrender.com/api/',
 });
 
 const path2 = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://cum-api-rest.onrender.com/api/',
   headers: {
     'Content-Type': 'multipart/form-data',
   },
 });
 
 const path3 = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://cum-api-rest.onrender.com/api/',
 });
 
 // Agregar el interceptor para modificar las solicitudes salientes
 path.interceptors.request.use(async (config) => {
   const token = await getTokenValue(); // Obtén el valor actualizado del token de las cookies
-
 
   if (token && token !== undefined) {
     config.headers.Authorization = `Token ${token}`;
@@ -32,7 +31,6 @@ path.interceptors.request.use(async (config) => {
 
 path2.interceptors.request.use(async (config) => {
   const token = await getTokenValue(); // Obtén el valor actualizado del token de las cookies
-
 
   if (token && token !== undefined) {
     config.headers.Authorization = `Token ${token}`;
